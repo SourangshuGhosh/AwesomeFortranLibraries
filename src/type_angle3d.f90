53e94fbbfab7fe772f58506b265b6b4fc100eaca
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-30 19:53:01 +0530

    Update done by Sourangshu Ghosh

--- src/type_angle3d.f90
+++ src/type_angle3d.f90
@@ -2,7 +2,7 @@
 ! 3D angle related routines
 !
 ! ------------------------------------------------------------------------------
-! Copyright (c) 2009-13, Thomas P. Robitaille
+! Copyright (c) 2020, Sourangshu Ghosh
 !
 ! All rights reserved.
 !
