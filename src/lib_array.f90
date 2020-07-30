7d97258e461a37f6a2ea2610d4220b44c1917d03
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-30 19:36:25 +0530

    Update Done by Sourangshu Ghosh

--- src/lib_array.f90
+++ src/lib_array.f90
@@ -2,7 +2,7 @@
 ! Array related routines (Integration, Interpolation, etc.)
 !
 ! ------------------------------------------------------------------------------
-! Copyright (c) 2009-13, Thomas P. Robitaille
+! Copyright (c) 2020 , Sourangshu Ghosh
 !
 ! All rights reserved.
 !
