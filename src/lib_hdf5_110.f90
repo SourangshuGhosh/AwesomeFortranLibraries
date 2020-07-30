c25532fc3f51419db38bde9df11a943f56aa7405
Sourangshu Ghosh <sourangshug123@gmail.com>
2020-07-30 19:40:08 +0530

    Update done by Sourangshu Ghosh

--- src/lib_hdf5_110.f90
+++ src/lib_hdf5_110.f90
@@ -2,7 +2,7 @@
 ! High level routines for HDF5
 !
 ! ------------------------------------------------------------------------------
-! Copyright (c) 2009-13, Thomas P. Robitaille
+! Copyright (c) 2020, Sourangshu Ghosh
 !
 ! All rights reserved.
 !
